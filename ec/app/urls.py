5ba61d7aa551738caec47d09a18f81e59da057cc
Saran P S <saranps007@gmail.com>
2023-05-09 22:29:08 +0530

    Changes : 	modified:   ec/app/__pycache__/urls.cpython-311.pyc 	modified:   ec/app/__pycache__/views.cpython-311.pyc 	modified:   ec/app/templates/app/addtocart.html 	modified:   ec/app/templates/app/base.html 	modified:   ec/app/templates/app/productdetail.html 	modified:   ec/app/urls.py 	modified:   ec/app/views.py 	modified:   ec/db.sqlite3

--- ec/app/urls.py
+++ ec/app/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
 
     path('add_to_cart/',views.add_to_cart,name='add_to_cart'),
     path('cart/',views.show_cart,name='showcart'),
+    path('checkout/',views.show_cart,name='checkout'),
     
     path('registration/',views.CustomerRegistrationView.as_view(),name='customerregistration'),
     path('accounts/login/',auth_view.LoginView.as_view(template_name='app/login.html',authentication_form=LoginForm), name='login'),
@@ -29,7 +30,5 @@ urlpatterns = [
     path('password-reset/done/',auth_view.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/',auth_view.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html',form_class=MySetPasswordForm), name='password_reset_confirm'),
     path('password-reset-complete/',auth_view.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'), name='password_reset_complete'),
-
-
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ec/app/views.py
+++ ec/app/views.py
@@ -99,10 +99,15 @@ def add_to_cart(request):
     user=request.user
     product_id=request.GET.get('prod_id')
     product=Product.objects.get(id=product_id)
-    Cart(user=user,product=product).save
+    Cart(user=user,product=product).save()
     return redirect('/cart')
 
 def show_cart(request):
     user=request.user
     cart=Cart.objects.filter(user=user)
-    return render(request,'app/addtocart.html',locals())
+    amount=0
+    for p in cart:
+        value=p.quantity*p.product.discounted_price
+        amount=amount+value
+    totalamount=amount+40
+    return render(request,'app/addtocart.html',locals()) 
