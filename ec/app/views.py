aaf5efbe7a064ab6f4833841b7e950b893ac619d
Saran P S <saranps007@gmail.com>
2023-05-22 21:43:41 +0530

    modified:   ec/app/__pycache__/views.cpython-311.pyc 	modified:   ec/app/templates/app/checkout.html 	modified:   ec/app/views.py 	modified:   ec/db.sqlite3

--- ec/app/views.py
+++ ec/app/views.py
@@ -115,8 +115,14 @@ def show_cart(request):
 class checkout(View):
     def get(self,request):
         user=request.user
-        add=Customer.objects.filter
-        return render(request,'app/checkout.html')
+        add=Customer.objects.filter(user=user)
+        cart_items=Cart.objects.filter(user=user)
+        famount=0
+        for p in cart_items:
+            value=p.quantity*p.product.discounted_price
+            famount=famount+value
+        totalamount=famount+40
+        return render(request,'app/checkout.html',locals())
 
 def plus_cart(request):
     if request.method=='GET':
